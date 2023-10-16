3d7986de1cfd1c0e21dd0b5859f372f74cac63cf
coder-guy1 <abhijeetingh11705@gmail.com>
2023-10-16 15:50:34 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-// Select the input element and the button element
-const inputElement = document.querySelector(".input");
-const buttonElement = document.querySelector(".abc");
+// Get all trash icons in the document
+const trashIcons = document.querySelectorAll(".fa-trash-alt");
 
-// Add click event listener to the button
-buttonElement.addEventListener("click", function () {
-  // Get the value from the input element
-  const inputValue = inputElement.value;
+// Add click event listener to each trash icon
+trashIcons.forEach((trashIcon) => {
+  trashIcon.addEventListener("click", function () {
+    // Show a confirmation dialog
+    const isConfirmed = confirm("Are you sure you want to delete this todo?");
 
-  // Log the input value to the console (you can do any other operation with this value)
-  console.log("Input Value:", inputValue);
+    // If user confirms, delete the todo item
+    if (isConfirmed) {
+      const listItem = this.parentElement.parentElement;
+      listItem.remove();
+    }
+  });
+});
+
+// Add functionality to add new todos
+const todoInput = document.getElementById("12");
+const todoList = document.querySelector("ul");
+
+todoInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter" && todoInput.value.trim() !== "") {
+    // Create a new li element
+    const newTodo = document.createElement("li");
+    newTodo.innerHTML = `<span><i class="far fa-trash-alt"></i></span> ${todoInput.value.trim()}`;
+
+    // Add click event listener to the new trash icon
+    const trashIcon = newTodo.querySelector(".fa-trash-alt");
+    trashIcon.addEventListener("click", function () {
+      // Show a confirmation dialog
+      const isConfirmed = confirm("Are you sure you want to delete this todo?");
+
+      // If user confirms, delete the todo item
+      if (isConfirmed) {
+        const listItem = this.parentElement.parentElement;
+        listItem.remove();
+      }
+    });
+
+    // Append the new li element to the ul
+    todoList.appendChild(newTodo);
+
+    // Clear the input field
+    todoInput.value = "";
+  }
 });
